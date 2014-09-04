f4b24b989de36a22430689ae60e6719fb8f8c568
luxurioust <xuri.me@gmail.com>
2014-09-04 10:32:16 +0800

    update analitycs code

--- app/views/system/analitycs.blade.php
+++ app/views/system/analitycs.blade.php
@@ -1,4 +1,18 @@
 {{-- CNZZ Analitycs code --}}
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1252933521'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/stat.php%3Fid%3D1252933521' type='text/javascript'%3E%3C/script%3E"));
+<script type="text/javascript">
+	// var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1252933521'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/stat.php%3Fid%3D1252933521' type='text/javascript'%3E%3C/script%3E"));
 </script>
 {{-- CNZZ Analitycs code --}}
+
+{{-- Google Analitycs code --}}
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42792257-6', 'auto');
+  ga('send', 'pageview');
+
+</script>
+{{-- Google Analitycs code --}}
