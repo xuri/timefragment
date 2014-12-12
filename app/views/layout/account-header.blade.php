87293102a3100517e2b9c930c7c199611bc24e6e
Luxurioust <xuri.me@gmail.com>
2014-12-12 09:40:51 +0800

    app icons retina support

--- app/views/layout/account-header.blade.php
+++ app/views/layout/account-header.blade.php
@@ -20,11 +20,25 @@
 		<meta name="apple-mobile-web-app-capable" content="yes"/>
 		{{-- Favicons --}}
 
-		<link rel="shortcut icon" href="{{ route('home')}}/images/icon/ico-16.ico" sizes="16x16">
-		<link rel="apple-touch-icon" href="{{ route('home')}}/images/icon/ico-57.png" sizes="57x57">
-		<link rel="apple-touch-icon" href="{{ route('home')}}/images/icon/ico-72.png" sizes="72x72">
-		<link rel="apple-touch-icon" href="{{ route('home')}}/images/icon/ico-114.png" sizes="114x114">
-		<link rel="apple-touch-icon" href="{{ route('home')}}/images/icon/ico-144.png" sizes="144x144">
+		{{-- For Chrome for Android: --}}
+		<link rel="icon" sizes="192x192" href="{{ route('home') }}/images/icons/touch-icon-192x192.png">
+		{{-- For iPhone 6 Plus with @3× display: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="180x180" href="{{ route('home')}}/images/icons/apple-touch-icon-180x180-precomposed.png">
+		{{-- For iPad with @2× display running iOS ≥ 7: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ route('home')}}/images/icons/apple-touch-icon-152x152-precomposed.png">
+		{{-- For iPad with @2× display running iOS ≤ 6: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ route('home')}}/images/icons/apple-touch-icon-144x144-precomposed.png">
+		{{-- For iPhone with @2× display running iOS ≥ 7: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ route('home')}}/images/icons/apple-touch-icon-120x120-precomposed.png">
+		{{-- For iPhone with @2× display running iOS ≤ 6: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ route('home')}}/images/icons/apple-touch-icon-114x114-precomposed.png">
+		{{-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ route('home')}}/images/icons/apple-touch-icon-76x76-precomposed.png">
+		{{-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: --}}
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ route('home')}}/images/icons/apple-touch-icon-72x72-precomposed.png">
+		{{-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: --}}
+		<link rel="apple-touch-icon-precomposed" href="{{ route('home')}}/images/icons/apple-touch-icon-precomposed.png">{{-- 57×57px --}}
+
 		{{ HTML::style('assets/css/fonts.googleapis.min.css') }}
 		{{ HTML::style('assets/css/admin.min.css') }}
 		{{ style('font-awesome-4.1.0') }}
