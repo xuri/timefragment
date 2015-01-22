b3f872056643b7fdcf4266f78751b4ca210338dc
Luxurioust <xuri.me@gmail.com>
2015-01-22 16:49:06 +0800

    update comments

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -6,7 +6,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @uses        Laravel The PHP frameworks for web artisans http://laravel.com
- * @author      Ri Xu http://xuri.me
+ * @author      Ri Xu http://xuri.me <xuri.me@gmail.com>
  * @copyright   Copyright (c) TimeFragment
  * @link        http://www.timefragment.com
  * @since       25th Nov, 2014
--- app/controllers/Admin/TravelCategoriesResource.php
+++ app/controllers/Admin/TravelCategoriesResource.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @uses        Laravel The PHP frameworks for web artisans http://laravel.com
+ * @author      Ri Xu http://xuri.me <xuri.me@gmail.com>
+ * @copyright   Copyright (c) TimeFragment
+ * @link        http://www.timefragment.com
+ * @since       25th Nov, 2014
+ * @license     Licensed under The MIT License http://www.opensource.org/licenses/mit-license.php
+ * @version     0.1
+ */
+
 class Admin_TravelCategoriesResource extends BaseResource
 {
 	/**
