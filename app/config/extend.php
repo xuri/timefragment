3852083d6e893c57672f7e30e5155966d3570374
Luxurioust <xuri.me@gmail.com>
2015-05-18 07:22:51 +0800

    travis CI test#1

--- app/config/extend.php
+++ app/config/extend.php
@@ -9,57 +9,57 @@
 
 return array(
 
-	/**
-	 * Website static aliases asset
-	 */
-	'webAssets' => array(
+    /**
+     * Website static aliases asset
+     */
+    'webAssets' => array(
 
-		'cssAliases' => array(  //  Stylesheet aliases asset
-			'cdn::bootstrap' 					=> '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
-			'bootstrap-3-switch'				=> 'assets/bootstrap-3-switch/css/bootstrap-switch.min.css',
-			'bootstrap-select'					=> 'assets/bootstrap-select/css/bootstrap-select.min.css',
-			'bootstrap-markdown'				=> 'assets/bootstrap-markdown/css/bootstrap-markdown.min.css',
-			'cdn::font-awaresome'		 		=> '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
-			'flexslider-2.2'					=> 'assets/flexslider-2.2/flexslider.min.css',
-			'normalize-3.0.1'					=> 'assets/normalize-3.0.1/normalize-3.0.1.min.css',
-			'jquery-file-upload'				=> 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload.css',
-			'jquery-file-upload-ui'				=> 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-ui.css',
-			'jquery-file-upload-noscript'		=> 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-noscript.css',
-			'jquery-file-upload-ui-noscript'	=> 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-ui-noscript.css',
-		),
+        'cssAliases' => array(  //  Stylesheet aliases asset
+            'cdn::bootstrap'                    => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
+            'bootstrap-3-switch'                => 'assets/bootstrap-3-switch/css/bootstrap-switch.min.css',
+            'bootstrap-select'                  => 'assets/bootstrap-select/css/bootstrap-select.min.css',
+            'bootstrap-markdown'                => 'assets/bootstrap-markdown/css/bootstrap-markdown.min.css',
+            'cdn::font-awaresome'               => '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
+            'flexslider-2.2'                    => 'assets/flexslider-2.2/flexslider.min.css',
+            'normalize-3.0.1'                   => 'assets/normalize-3.0.1/normalize-3.0.1.min.css',
+            'jquery-file-upload'                => 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload.css',
+            'jquery-file-upload-ui'             => 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-ui.css',
+            'jquery-file-upload-noscript'       => 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-noscript.css',
+            'jquery-file-upload-ui-noscript'    => 'assets/jquery-file-upload-9.5.7/css/jquery.fileupload-ui-noscript.css',
+        ),
 
-		'jsAliases'  => array(  //  Script aliases asset
-			// jQuery Lib
-			'cdn::jquery'					=> '//code.jquery.com/jquery-2.1.3.min.js',
-			'cdn::bootstrap'				=> '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js',
-			'bootstrap-3-switch'			=> 'assets/bootstrap-3-switch/js/bootstrap-switch.min.js',
-			'bootstrap-select'				=> 'assets/bootstrap-select/js/bootstrap-select.min.js',
-			'markdown'						=> 'assets/bootstrap-markdown/js/markdown.js',
-			'to-markdown'					=> 'assets/bootstrap-markdown/js/to-markdown.js',
-			'bootstrap-markdown'			=> 'assets/bootstrap-markdown/js/bootstrap-markdown.js',
-			'flexslider-2.2'				=> 'assets/flexslider-2.2/jquery.flexslider-min.js',
-			'modernizr-2.8.1'				=> 'assets/modernizr-2.8.1/modernizr-2.8.1.min.js',
-			'html5shiv-3.7.0'				=> 'assets/html5shiv-3.7.0/jhtml5shiv-3.7.0.min.js',
-			'html5shiv-printshiv-3.7.0'		=> 'assets/html5shiv-3.7.0/html5shiv-printshiv-3.7.0.min.js',
-			'jquery-ui-widget'				=> 'assets/jquery-file-upload-9.5.7/js/vendor/jquery.ui.widget.js',
-			'jquery-iframe-transport'		=> 'assets/jquery-file-upload-9.5.7/js/jquery.iframe-transport.js',
-			'jquery-file-upload'			=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload.js',
-			'jquery-file-upload-process'	=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-process.js',
-			'jquery-file-upload-image'		=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-image.js',
-			'jquery-file-upload-audio'		=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-audio.js',
-			'jquery-file-upload-video'		=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-video.js',
-			'jquery-file-upload-validate'	=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-validate.js',
-			'jquery-file-upload-ui'			=> 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-ui.js',
-			'jquery-file-upload-main'		=> 'assets/jquery-file-upload-9.5.7/js/main.js',
-			'jquery-xdr-transport'			=> 'assets/jquery-file-upload-9.5.7/js/corsjquery.xdr-transport.js',
-			'tmpl'							=> 'assets/jquery-file-upload-9.5.7/js/tmpl.min.js',
-			'load-image'					=> 'assets/jquery-file-upload-9.5.7/js/load-image.min.js',
-			'canvas-to-blob'				=> 'assets/jquery-file-upload-9.5.7/js/canvas-to-blob.min.js',
-			'jquery-blueimp-gallery'		=> 'assets/jquery-file-upload-9.5.7/js/jquery.blueimp-gallery.min.js',
-			'ckeditor'						=> 'assets/ckeditor-4.4.1/ckeditor.js',
-			'analytics'						=> 'assets/js/analytics.min.js',
-		),
+        'jsAliases'  => array(  //  Script aliases asset
+            // jQuery Lib
+            'cdn::jquery'                   => '//code.jquery.com/jquery-2.1.3.min.js',
+            'cdn::bootstrap'                => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js',
+            'bootstrap-3-switch'            => 'assets/bootstrap-3-switch/js/bootstrap-switch.min.js',
+            'bootstrap-select'              => 'assets/bootstrap-select/js/bootstrap-select.min.js',
+            'markdown'                      => 'assets/bootstrap-markdown/js/markdown.js',
+            'to-markdown'                   => 'assets/bootstrap-markdown/js/to-markdown.js',
+            'bootstrap-markdown'            => 'assets/bootstrap-markdown/js/bootstrap-markdown.js',
+            'flexslider-2.2'                => 'assets/flexslider-2.2/jquery.flexslider-min.js',
+            'modernizr-2.8.1'               => 'assets/modernizr-2.8.1/modernizr-2.8.1.min.js',
+            'html5shiv-3.7.0'               => 'assets/html5shiv-3.7.0/jhtml5shiv-3.7.0.min.js',
+            'html5shiv-printshiv-3.7.0'     => 'assets/html5shiv-3.7.0/html5shiv-printshiv-3.7.0.min.js',
+            'jquery-ui-widget'              => 'assets/jquery-file-upload-9.5.7/js/vendor/jquery.ui.widget.js',
+            'jquery-iframe-transport'       => 'assets/jquery-file-upload-9.5.7/js/jquery.iframe-transport.js',
+            'jquery-file-upload'            => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload.js',
+            'jquery-file-upload-process'    => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-process.js',
+            'jquery-file-upload-image'      => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-image.js',
+            'jquery-file-upload-audio'      => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-audio.js',
+            'jquery-file-upload-video'      => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-video.js',
+            'jquery-file-upload-validate'   => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-validate.js',
+            'jquery-file-upload-ui'         => 'assets/jquery-file-upload-9.5.7/js/jquery.fileupload-ui.js',
+            'jquery-file-upload-main'       => 'assets/jquery-file-upload-9.5.7/js/main.js',
+            'jquery-xdr-transport'          => 'assets/jquery-file-upload-9.5.7/js/corsjquery.xdr-transport.js',
+            'tmpl'                          => 'assets/jquery-file-upload-9.5.7/js/tmpl.min.js',
+            'load-image'                    => 'assets/jquery-file-upload-9.5.7/js/load-image.min.js',
+            'canvas-to-blob'                => 'assets/jquery-file-upload-9.5.7/js/canvas-to-blob.min.js',
+            'jquery-blueimp-gallery'        => 'assets/jquery-file-upload-9.5.7/js/jquery.blueimp-gallery.min.js',
+            'ckeditor'                      => 'assets/ckeditor-4.4.1/ckeditor.js',
+            'analytics'                     => 'assets/js/analytics.min.js',
+        ),
 
-	),
+    ),
 
 );
